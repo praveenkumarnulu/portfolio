426d85c13ed359989c4ba09712859e64f166d175
nnvvpk <nnvvpk@gmail.com>
2018-06-21 13:07:59 +0000

    Created jobs home page with bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# title
+    # pub_date
+    # body
+    # image
+class blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
